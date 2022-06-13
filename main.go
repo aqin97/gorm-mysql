9533374ca074ae6b76c9931803c1d62e7ca88abf
aqin97 <791041423@qq.com>
2022-06-13 23:43:08 +0800

    .

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gorm-mysql/setting"
 	"log"
 
 	"github.com/jinzhu/gorm"
@@ -15,19 +16,31 @@ type User struct {
 	Password string
 }
 
+var DBSetting *setting.DBsetting
+
+func init() {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		log.Fatal("--init setting err:--", err)
+	}
+	setting.ReadSection("DataBase", &DBSetting)
+}
+
 func main() {
-	db, err := gorm.Open("mysql", "root:root@tcp(localhost:3306)/seckill?charset=utf8mb4&parseTime=True&loc=Local")
+	//db, err := gorm.Open("mysql", "root:root@tcp(localhost:3306)/seckill?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", DBSetting.DataSource)
+
 	if err != nil {
 		log.Fatal("main.init err:", err)
 	}
 	defer db.Close()
 	//*********************************在这里进行练习*********************************//
-	var users = []User{}
-	res := db.Exec("update users set password = ? where id = ?", "123456", 1)
+	var user []User
+	res := db.Select("username").Find(&user)
 	//*********************************在这里进行练习*********************************//
 	if res.Error != nil {
 		log.Fatal("create fail err:", res.Error)
 	}
 	fmt.Println(res.RowsAffected, res.Value)
-	fmt.Println(users)
+	fmt.Println(user)
 }
